6090e4b5c8223b50ccc71f8379af56adab2430b5
pas8 <peterpreneur@gmail.com>
2022-01-27 19:19:16 -0600

    test

--- src/main/java/streamsdemo/StreamsDemoAplication.java
+++ src/main/java/streamsdemo/StreamsDemoAplication.java
@@ -1,5 +1,23 @@
 package streamsdemo;
 
-public class StreamsDemoAplication {
+import java.sql.Date;
+import java.util.stream.IntStream;
 
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class StreamsDemoAplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StreamsDemoApplication.class, args);
+		
+		//Date start = new Date();
+		//IntStream.range(0,1000).parallel().forEach(i->System.out.prinln(i));
+		//Date end = new Date();
+		//long length = end.getTime() - start.getTime();
+		
+		Date start = new Date();
+		
+		
+	}
 }
